380a249af4c74923b97b1e9ac2da9dc88616499a
Nelson Vui Lim Bong <nelsonvuilimbong@Mac.home>
2025-06-06 14:17:30 +0800

    Update styles and add dropdown.png image

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,21 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/dropdown.png'
 
 const Navbar = () => {
 
     const [menu,setMenu] = useState("shop")
     const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
 
   return (
     <div className='navbar'>
@@ -16,11 +23,12 @@ const Navbar = () => {
             <img src={logo} alt="" width={70} height={70}/>
             <p>Tien Hiong Ent.</p>
         </div>
-        <div className="nav-menu">
+        <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+        <ul ref={menuRef} className="nav-menu">
             <li onClick={()=>setMenu("shop")}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
             <li onClick={()=>setMenu("tea")}><Link style={{ textDecoration: 'none' }} to='/tea'>Tea</Link>{menu==="tea"?<hr/>:<></>}</li>
             <li onClick={()=>setMenu("coffee")}><Link style={{ textDecoration: 'none' }} to='/coffee'>Coffee</Link>{menu==="coffee"?<hr/>:<></>}</li>
-        </div>
+        </ul>
         <div className="nav-login-cart">
             {localStorage.getItem('auth-token')
             ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
